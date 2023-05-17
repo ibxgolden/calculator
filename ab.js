7066628f3072d5d7c98db7ddabdeb669ca667738
ibxgolden <129530843+ibxgolden@users.noreply.github.com>
2023-05-17 12:16:42 +0300

    Create ab.js
    when the btn is clicked what happened? to answer this i performed some function for the pause start and reset btn

---
+++ ab.js
@@ -0,0 +1,20 @@
+let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+let timerRef = document.querySelector('.timerDisplay');
+let int = null;
+
+document.getElementById('startTimer').addEventListener('click', ()=>{
+    if(int!==null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer,10);
+});
+
+document.getElementById('pauseTimer').addEventListener('click', ()=>{
+    clearInterval(int);
+});
+
+document.getElementById('resetTimer').addEventListener('click', ()=>{
+    clearInterval(int);
+    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+    timerRef.innerHTML = '00 : 00 : 00 : 000 ';
+});
